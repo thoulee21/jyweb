331dfb6af8849d9dcbab7cb1efef2f43dc38b90b
胡启峰 <2690167596@qq.com>
2024-02-25 17:47:27 +0800

    feat：关于我们页面开发

---
+++ src/views/product/Accordion/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { accordionData } from "./index.config";
+import "./index.scss";
+function Accordion(props) {
+  const [active, setActive] = useState();
+  return (
+    <div
+      className="sfq"
+      onMouseLeave={() => {
+        setActive("");
+      }}
+    >
+      {accordionData.map((item) => {
+        return (
+          <div
+            key={item.title}
+            className={`sfq-item ${active == item.title ? "active" : ""} ${
+              item.icon
+            }`}
+            onMouseEnter={() => setActive(item.title)}
+          >
+            <div className="sfq-title">{item.title}</div>
+            <div className="sfq-synopsis">{item.synopsis}</div>
+            <div className="sfq-content">
+              <ul>
+                {item.content.split("/").map((val) => {
+                  return <li key={val}>{val}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Accordion;
--- src/views/product/index.jsx
+++ src/views/product/index.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-
+import people from "../../assets/images/people.png";
+import Accordion from "./Accordion/index";
+import { tableLIst } from "./Accordion/index.config";
+import "./index.scss";
 function Product(props) {
   return (
     <div className="product-page">
+      <div className="parduct-page-top">
+        <div className="top-left">
+          <div className="product">优质产品</div>
+          <div className="service">服务至上</div>
+        </div>
+        <div className="top-right">
+          <img src={people} alt="" />
+        </div>
+      </div>
+      <div className="parduct-page-middle">
+        <div className="middle-title">我们的产品</div>
+        <Accordion></Accordion>
+        <div className="middle-list">
+          {tableLIst.map((item) => {
+            return (
+              <div className="list-item" key={item.title}>
+                <div className="list-title">{item.title}</div>
+                <div className="list-content">{item.content}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="parduct-page-middle-buttom">点击咨询</div>
+      </div>
+      <div className="parduct-page-bottom">
+        <div className="parduct-page-bottom-title">新闻资讯/政府资料</div>
+        <div className="parduct-page-bottom-contet">
+          <div className="empty"></div>
+          <div className={`empty one`}></div>
+          <div className="empty"></div>
+        </div>
+      </div>
     </div>
   );
 }
