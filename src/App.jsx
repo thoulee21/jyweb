c5251a055bbc0791a2fca11873b45edbf369c227
胡启峰 <2690167596@qq.com>
2024-02-28 12:39:24 +0800

    feat:样式修改

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import {NavLink, useRoutes} from "react-router-dom";
+import { NavLink, useRoutes, useLocation } from "react-router-dom";
 import routes from "./router";
 import "./App.scss";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 
 function App() {
   const menus = [
-    {path: "/", name: "首页"},
-    {path: "/about", name: "关于我们"},
-    {path: "/product", name: "产品/服务"},
-    {path: "/news", name: "新闻资讯"},
-    {path: "/contact", name: "联系我们"}
+    { path: "/", name: "首页" },
+    { path: "/about", name: "关于我们" },
+    { path: "/product", name: "产品/服务" },
+    { path: "/news", name: "新闻资讯" },
+    { path: "/contact", name: "联系我们" },
   ];
   const [scrolling, setScrolling] = useState(false);
-  const activeClassNameFn = ({isActive}) => isActive ? "nav-item active" : "nav-item";
-  
+  // const [propporty, setProporty] = useState(1);
+  const activeClassNameFn = ({ isActive }) =>
+    isActive ? "nav-item active" : "nav-item";
+  const [locat, setLocat] = useState();
+  const location = useLocation();
   useEffect(() => {
     const onScroll = () => setScrolling(document.documentElement.scrollTop > 0);
     window.addEventListener("scroll", onScroll);
+
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
   }, []);
-  
+  // useEffect(() => {
+  //   const zoom = document.documentElement.clientWidth / 1920;
+  //   if (location.pathname.split("/")[1]) {
+  //     document.body.style.zoom = zoom;
+  //   } else {
+  //     document.body.style.zoom = 1;
+  //   }
+  // }, [document.documentElement.clientWidth, location]);
+  useEffect(() => {
+    const data = location.pathname.split("/")[1];
+    setLocat(data);
+    document.documentElement.scrollTop = 0;
+  }, [location]);
+
   return (
     <div className="layout">
-      <div className={classNames("layout-header", {scrolling})}>
+      <div
+        className={classNames("layout-header", {
+          scrolling,
+          [locat]: scrolling ? false : locat,
+        })}
+      >
         <div className="logo">
-          <img src="http://localhost:3000/logo192.png" alt="logo"/>
+          <img src="http://localhost:3000/logo192.png" alt="logo" />
           <span className="text">君颖投资</span>
         </div>
         <nav className="nav">
@@ -36,24 +58,22 @@ function App() {
               <NavLink
                 to={item.path}
                 key={item.path}
-                className={activeClassNameFn}>
+                className={activeClassNameFn}
+              >
                 {item.name}
               </NavLink>
             );
-            
           })}
         </nav>
       </div>
-      <div className="layout-body">
-        {useRoutes(routes)}
-      </div>
+      <div className="layout-body">{useRoutes(routes)}</div>
       <div className="layout-footer">
         <nav className="nav">
-          {
-            menus.map((item) => (
-              <NavLink to={item.path} key={item.path}>{item.name}</NavLink>
-            ))
-          }
+          {menus.map((item) => (
+            <NavLink to={item.path} key={item.path}>
+              {item.name}
+            </NavLink>
+          ))}
         </nav>
         <div className="extra">
           <NavLink to="/follow">关注我们</NavLink>
--- src/views/contact/index.jsx
+++ src/views/contact/index.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import linkRight from "../../assets/images/link-right.jpg";
 import dianhua from "../../assets/images/dianhua.svg";
 import lianxiwomen from "../../assets/images/lianxiwomen.svg";
 import "./index.scss";
 
 function Contact(props) {
+  const [anm, setAnm] = useState(false);
+  useEffect(() => {
+    const onScroll = () => {
+      const scrollPosition = window.scrollY;
+      console.log(scrollPosition, "scrollPosition");
+      if (scrollPosition > 200) {
+        setAnm(true);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
   return (
     <div className="contact-page">
       <div className="contact-page-top">
@@ -24,9 +38,17 @@ function Contact(props) {
         <div className="middle-content">
           <div className="middle-content-left">
             <div className="title">免费*快速为您注册公司</div>
-            <input placeholder="公司名称" className="comput-name"></input>
-            <input placeholder="预留电话" className="tell-number"></input>
-            <div className="link-people">快速为您匹配职业经理人</div>
+            <input
+              placeholder="公司名称"
+              className={`comput-name ${anm ? "anm-comput" : ""}`}
+            ></input>
+            <input
+              placeholder="预留电话"
+              className={`tell-number ${anm ? "anm-tell" : ""}`}
+            ></input>
+            <div className={`link-people ${anm ? "anm-link" : ""}`}>
+              快速为您匹配职业经理人
+            </div>
           </div>
           <div className="middle-content-right">
             <img src={linkRight} alt="" />
